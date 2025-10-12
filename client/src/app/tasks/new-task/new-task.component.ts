f36aed3b23bbb2540928791ad8eed5122a585e14
Jan Zelinka <jan.zelinka12@gmail.com>
2025-10-12 19:07:59 +0200

    two model binding

--- client/src/app/tasks/new-task/new-task.component.ts
+++ client/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.less',
 })
 export class NewTaskComponent {
   @Output() onCloseModal = new EventEmitter();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   closeModal() {
     this.onCloseModal.emit();
