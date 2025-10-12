85d9745f96b4dd1354c958923131aab9b54e6c95
Jan Zelinka <jan.zelinka12@gmail.com>
2025-10-12 09:35:00 +0200

    signal computed property

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,9 +12,10 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = signal(DUMMY_USERS[randomIndex]);
-  get selectedUserAvatar() {
+
+  imagePath = computed(() => {
     return 'assets/users/' + this.selectedUser().avatar;
-  }
+  });
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
