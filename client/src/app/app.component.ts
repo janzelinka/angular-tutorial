e8908a5a1727ec5d86ceb4458260567cf09a963c
Jan Zelinka <jan.zelinka12@gmail.com>
2025-10-12 10:02:25 +0200

    component inputs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'client';
+
+  users = DUMMY_USERS;
 }
