7dfdeb396e2ca2420687b491cb04a63ae61a8b00
Jan Zelinka <jan.zelinka12@gmail.com>
2025-10-14 20:27:34 +0200

    tasks module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
-import { TaskComponent } from './tasks/task/task.component';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TasksComponent,
-    UserComponent,
-    NewTaskComponent,
-    TaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, DatePipe, SharedModule],
+  imports: [BrowserModule, CommonModule, SharedModule, TasksModule],
 })
 export class AppModule {}
