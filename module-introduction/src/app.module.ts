1535280418a8f1193fb3b0d86e51ddd4fc3dbb35
Jan Zelinka <jan.zelinka12@gmail.com>
2025-10-16 19:25:36 +0200

    user input component

--- module-introduction/src/app.module.ts
+++ module-introduction/src/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './app/header/header.component';
 import { AppComponent } from './app/app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserInputComponent } from './app/user-input/user-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HeaderComponent, AppComponent],
+  declarations: [HeaderComponent, AppComponent, UserInputComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
--- module-introduction/src/app/user-input/user-input.component.ts
+++ module-introduction/src/app/user-input/user-input.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment: number = 0;
+  annualInvestment: number = 0;
+  expectedReturn: number = 0;
+  duration: number = 0;
 
+  handleSubmit() {
+    console.log(
+      this.initialInvestment,
+      this.annualInvestment,
+      this.expectedReturn,
+      this.duration
+    );
+  }
 }
