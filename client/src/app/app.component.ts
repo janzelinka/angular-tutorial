e09a7fcd3497a0c91d333b636392b705aa542d9e
jank0 <jan.zelinka12@gmail.com>
2025-11-22 08:55:22 +0100

    initial commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,37 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { CommonModule } from '@angular/common';
-import { TasksComponent } from './tasks/tasks.component';
-
-export interface IUser {
-  id: string;
-  name: string;
-  avatar: string;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    UserComponent,
-    CommonModule,
-    TasksComponent,
-  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less',
 })
 export class AppComponent {
-  title = 'client';
-
-  users: IUser[] = DUMMY_USERS;
-  selectedUser?: IUser;
-
-  onSelectUser(userId: string) {
-    this.selectedUser = this.users.find((user) => user.id === userId);
-  }
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
